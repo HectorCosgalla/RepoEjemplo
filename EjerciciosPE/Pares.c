14ea6ee4a059d9240a60b93c5a4401b373e36ab9
HectorCosgalla <46679850+HectorCosgalla@users.noreply.github.com>
2020-01-16 12:55:04 -0600

    Update Pares.c

--- EjerciciosPE/Pares.c
+++ EjerciciosPE/Pares.c
@@ -1,6 +1,13 @@
 #include "stdio.h"
 #include "conio.h"
 int main() {
-  printf("PUTOS TODOS uwu\n");
+  int Num, Des, ContPar, Div, DivPar, Num2;
+  Num = 0;
+  Num2 = 0;
+  Des = 0;
+  ContPar = 0;
+  Div = 10;
+  DivPar = 2;
+  
   return 0;
 }
