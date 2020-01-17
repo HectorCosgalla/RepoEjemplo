fed70fd05f80a8e81d28f0cae23192a71bd10fc9
HectorCosgalla <valcos98@hotmail.com>
2020-01-16 21:28:08 -0600

    Update Perfecto.c

--- EjerciciosPE/Perfecto.c
+++ EjerciciosPE/Perfecto.c
@@ -1,6 +1,23 @@
 #include "stdio.h"
 #include "conio.h"
 int main() {
-  printf("PUTOS TODOS XD\n");
-  return 0;
+  int Num, Sum, Cont;
+  Num = 0;
+  Sum = 0;
+  Cont = 1;
+  printf("Ingrese un numero (De preferencia perfecto):\n");
+  scanf("%d", &Num);
+  do {
+    if (Num%Cont == 0) {
+      Sum = Sum + Cont;
+    }
+    Cont = Cont + 1;
+  } while(Cont < Num);
+
+  if (Sum == Num) {
+    printf("El numero %d es perfecto\n", Num);
+  } else {
+    printf("El numero %d no es perfecto\n", Num);
+  }
+
 }
