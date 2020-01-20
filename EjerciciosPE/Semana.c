8830ee79f5888829ce9dfc3dcc568e1041998062
HectorCosgalla <46679850+HectorCosgalla@users.noreply.github.com>
2020-01-20 12:45:24 -0600

    Add files via upload

---
+++ EjerciciosPE/Semana.c
@@ -0,0 +1,32 @@
+#include "stdio.h"
+#include "conio.h"
+int main() {
+  int Dia;
+  Dia = 0;
+  printf("Ingrese el numero del dia\n");
+  scanf("%d", &Dia);
+  switch (Dia) {
+    case 1:
+      printf("El dia %d es domingo\n", Dia);
+      break;
+    case 2:
+      printf("El dia %d es lunes\n", Dia);
+      break;
+    case 3:
+      printf("El dia %d es martes\n", Dia);
+      break;
+    case 4:
+      printf("El dia %d es miercoles\n", Dia);
+      break;
+    case 5:
+      printf("El dia %d es jueves\n", Dia);
+      break;
+    case 6:
+      printf("El dia %d es viernes\n", Dia);
+      break;
+    case 7:
+      printf("El dia %d es sabado\n", Dia);
+      break;
+  }
+  return 0;
+}
