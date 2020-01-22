a6452f6343de27d7727b4815cf3d90723f3f9e15
HectorCosgalla <valcos98@hotmail.com>
2020-01-21 21:29:27 -0600

    dfxfc

---
+++ EjerciciosPE/Cronometro.c
@@ -0,0 +1,48 @@
+#include "stdio.h"
+#include "conio.h"
+
+int main() {
+  int a, i;
+  a = 0;
+  i = 0;
+
+  printf("          |================================|\n");
+  printf("          |  C  R  O  N  O  M  E  T  R  O  |\n");
+  printf("          |================================|\n");
+  printf("             Pulsa una tecla para empezar\n");
+  getchar();
+  /*for (int a = 0; a < 60; a++) {
+    for (int i = 1; i < 60; i++) {
+      system("cls");
+      printf("          |================================|\n");
+      printf("          |  C  R  O  N  O  M  E  T  R  O  |\n");
+      printf("          |================================|\n");
+      printf("                       %d : %d\n", a, i);
+      sleep(1);
+    }
+  }*/
+
+  while (getchar()) {
+    if (i < 60) {
+      i++;
+      system("cls");
+      printf("          |================================|\n");
+      printf("          |  C  R  O  N  O  M  E  T  R  O  |\n");
+      printf("          |================================|\n");
+      printf("                       %d : %d\n", a, i);
+      sleep(1);
+    } else{
+      i = 0;
+      a++;
+      system("cls");
+      printf("          |================================|\n");
+      printf("          |  C  R  O  N  O  M  E  T  R  O  |\n");
+      printf("          |================================|\n");
+      printf("                       %d : %d\n", a, i);
+      sleep(1);
+    }
+
+  }
+
+  return 0;
+}
