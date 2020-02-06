d3fc9ca979cd2aaf014271dfb5853699bd9f574b
HectorCosgalla <46679850+HectorCosgalla@users.noreply.github.com>
2020-02-06 12:43:02 -0600

    Add files via upload

---
+++ EjerciciosPE/gatomas.c
@@ -0,0 +1,26 @@
+#include "stdio.h"
+
+int main() {
+	int n;
+	printf("Ingresa el valor de n:\n");
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			if (i%2 == 0) {
+				if (j%2 == 0) {
+					printf("#");
+				} else {
+					printf("+");
+				}
+			} else {
+				if (j%2 == 0) {
+					printf("+");
+				} else {
+					printf("#");
+				}
+			}
+		}
+		printf("\n");
+	}
+	return 0;
+}
