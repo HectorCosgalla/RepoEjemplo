7cf905118f1d4f8034c4db3fec337fd2bb4f947d
HECTOR ABRAHAM VALDIVIESO COSGALLA <A18016335@inet.uady.mx>
2020-02-11 11:15:28 -0600

    aaaaaa

---
+++ EjerciciosPE/Repunit.c
@@ -0,0 +1,58 @@
+#include "stdio.h"
+
+/********** PROTOTIPO DE FUNCIONES **********/
+void Entradas(int *);
+void Proceso(int *, int *, int *, bool *);
+void Salida(bool);
+
+int main() {
+  /********** ENTRADAS **********/
+  int n, uno;
+  bool repunit;
+  n = 0;
+  uno = 0;
+  repunit = false;
+
+  Entradas(&n);
+  /********** PROCESOS **********/
+  Proceso(&n, &uno, &otro, &repunit)
+
+  /********** SALIDAS **********/
+  Salida(repunit);
+  return 0;
+}
+
+/********** FUNCIONES **********/
+void Entradas(int *n) {
+  printf("Ingrese un numero (Porfas, que sea repunit);\n");
+  scanf("%d", n);
+}
+
+void Proceso(int *n, int *uno, bool *repunit) {
+  int final, nums;
+  nums = 0;
+  final = *n;
+  do {
+    final = final / 10
+    nums++;
+  } while (final > 0)
+
+  do {
+    final = *n / 10:
+    *n = *n / 10;
+    if (final == 1) {
+      *uno = *uno + 1;
+    }
+  } while(n > 0);
+  if (nums == *uno) {
+    *repunit = true;
+  }
+}
+
+void Salida(bool repunit) {
+  if (repunit == true) {
+    printf("Si es repunit :D");
+  } else {
+    printf("Te dije que pongas un repunit :(\n");
+  }
+}
