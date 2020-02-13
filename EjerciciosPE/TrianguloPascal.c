6df906182cd5bf7128b788c1dbe16f8c39d7cd8f
HECTOR ABRAHAM VALDIVIESO COSGALLA <A18016335@inet.uady.mx>
2020-02-13 12:00:51 -0600

    Update

--- EjerciciosPE/TrianguloPascal.c
+++ EjerciciosPE/TrianguloPascal.c
@@ -1,32 +1,36 @@
+#include "stdio.h"
+
 /********** PROTOTIPO DE FUNCIONES **********/
 void Entradas(int *);
-void Proceso(int *, int *, bool *);
-void Salida(bool);
+void Proceso(int *,int * ,int *);
+void Salida();
 
-int main() {
+int main(){
   /********** ENTRADAS **********/
   int num1, num2, n;
   num1 = 1;
-  num2 = 1;
+  num2 = 0;
   n = 0;
   Entradas(&n);
   /********** PROCESOS **********/
   Proceso(&n, &num1, &num2);
   /********** SALIDAS **********/
+  return 0;
 }
 
 /********** FUNCIONES **********/
-void Entradas(int &n){
+void Entradas(int *n){
   printf("Ingrese el limite:\n");
   scanf("%d", n);
 }
 
-void Proceso(int &n, int &num1, int &num2) {
+void Proceso(int *n, int *num1, int *num2) {
   int NumMed;
   NumMed = 0;
   for (int i = 0; i <= *n; i++) {
     for (int j = 0; j < i; j++) {
-
+      printf("%d ", *num1);
     }
+    printf("\n");
   }
 }
