7b05c6c62f8f0f0925afebf9446ac307fe7d9136
HECTOR ABRAHAM VALDIVIESO COSGALLA <A18016335@inet.uady.mx>
2020-03-02 12:59:43 -0600

    Create Menu.c

---
+++ EjerciciosPE/EjerciciosFunciones/Menu.c
@@ -0,0 +1,102 @@
+#include "stdio.h"
+#include "stdbool.h"
+/********** PROTOTIPO DE FUNCIONES **********/
+void ImPar();
+void Perfecto();
+int PerfectoAnid(int);
+void Reverso();
+
+int main(){
+  /********** DECLARACION DE VARIABLES **********/
+  int opc;
+  bool band;
+  band = false;
+
+  /********** ENTRADAS **********/
+  printf("\n" );
+  printf("\n" );
+  printf("\n" );
+
+  /********** PROCESOS **********/
+  do {
+    scanf("%d", &opc);
+    /********** SALIDAS **********/
+    switch (opc) {
+      case 1:
+        ImPar();
+        band = true;
+        break;
+      case 2:
+        Perfecto();
+        band = true;
+        break;
+      case 3:
+        Reverso();
+        band = true;
+        break;
+      default:
+      printf("ERROR: LA OPCION ES INCORRECTA\n");
+      break;
+    }
+  } while(band == false);
+
+  return 0;
+}
+
+/********** FUNCIONES **********/
+void ImPar() {
+  int num;
+  for (int i = 0; i < 10; i++) {
+    printf("Ingresa un numero:\n");
+    scanf("%d", &num);
+    if (num % 2 == 0) {
+      printf("El numero \"%d\" es par.\n", num);
+    }else {
+      printf("El numero \"%d\" es impar.\n", num);
+    }
+  }
+}
+
+void Perfecto() {
+  int Num, Perf, i;
+  Perf = 0;
+  Num = 1;
+  i = 0;
+  printf("Los primeros 3 numeros perfecto son: \n");
+  do {
+    Num = Num + 1;
+    Perf = PerfectoAnid(Num);
+    if (Perf == Num) {
+      printf("%d, ", Num);
+      i++;
+    }
+  } while(i < 3);
+}
+
+int PerfectoAnid(int Num) {
+  int  Sum, Cont;
+  Sum = 0;
+  Cont = 1;
+  do {
+    if (Num%Cont == 0) {
+      Sum = Sum + Cont;
+    }
+    Cont = Cont + 1;
+  } while(Cont < Num);
+  return Sum;
+}
+
+void Reverso() {
+  int Num, Num2, Alm;
+  Num = 0;
+  Num2 = 0;
+  Alm = 0;
+  printf("Ingrese un numero:\n");
+  scanf("%d", &Num);
+  do {
+    Alm = Num % 10;
+    Num2 = (Num2 + Alm)*10;
+    Num = Num / 10;
+  } while(Num > 0);
+  printf("El reverso del numero es: %d \n", Num2);
+}
