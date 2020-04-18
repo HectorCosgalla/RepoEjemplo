bb97c614e06dc9f173a9fdf33a4ce94f4a3265e3
HectorCosgalla <46679850+HectorCosgalla@users.noreply.github.com>
2020-04-18 03:22:28 -0500

    Update ADA05.c

--- EjerciciosPE/ProyectoStrings/ADA05.c
+++ EjerciciosPE/ProyectoStrings/ADA05.c
@@ -7,7 +7,11 @@ int compararCadenas(char cad1[], char cad2[]);
 int encontrarCadena(char cad1[], char cad2[]);
 int longitudCadena(char cad1[]);
 void copiarCadenas(char cad1[], char cad2[]);
-int encontrarCaracter(char cad[], char *car);
+int encontrarCaracter(char cad[], char car);
+int esDigito(char car);
+int esMayuscula(char car);
+int esVocal(char car);
+int esConsanante(char car);
 
 int main(){
   /********** DECLARACION DE VARIABLES **********/
@@ -22,6 +26,9 @@ int main(){
     printf("3) Obtener la longitud de una cadena\n");
     printf("4) Copiar una cadena a otra\n");
     printf("5) Obtener la posicion de un caracter en una cadena\n");
+    printf("6) Verificar si un caracter es un digito\n");
+    printf("7) Verificar si un caracter es una letra mayuscula\n");
+    printf("8) Verificar si un caracter es una letra consonante\n");
     scanf("%d", &opc);
     if (opc > 10 && opc < 1) {
       printf("ERROR: Opcion invalida\n");
@@ -40,10 +47,25 @@ int main(){
       break;
     case 3:
       sol = longitudCadena(cad1);
+      break;
     case 4:
       copiarCadenas(cad1, cad2);
+      break;
     case 5:
-      sol = encontrarCaracter(cad1, &car);
+      sol = encontrarCaracter(cad1, car);
+      break;
+    case 6:
+      sol = esDigito(car);
+      break;
+    case 7:
+      sol = esMayuscula(car);
+      break;
+    case 8:
+      sol = esVocal(car);
+      break;
+    case 9:
+      sol = esConsanante(car);
+      break;
   }
 
   /********** SALIDAS **********/
@@ -77,7 +99,7 @@ void entradas(char cad1[], char cad2[], char *car, int opc){
     gets(cad2);
   }
 
-  if (opc == 3 || opc == 4) {
+  if (opc == 3 || opc == 4 || opc == 10) {
     printf("Ingrese una cadena: \n");
     fflush(stdin);
     gets(cad1);
@@ -87,6 +109,12 @@ void entradas(char cad1[], char cad2[], char *car, int opc){
     printf("Ingrese una cadena: \n");
     fflush(stdin);
     gets(cad1);
+    printf("Ingrese un caracter: \n");
+    fflush(stdin);
+    gets(car);
+  }
+
+  if (opc >= 6 && opc < 10) {
     printf("Ingrese un caracter: \n");
     fflush(stdin);
     gets(car);
@@ -153,13 +181,47 @@ void copiarCadenas(char cad1[], char cad2[]) {
   }
 }
 
-int encontrarCaracter(char cad1[], char *car) {
-  int i = 0, pos = 0;
+int encontrarCaracter(char cad1[], char car) {
+  int i = 0;
   while ( cad1[i] != '\0') {
-    if (cad1[i] == *car) {
-      pos = i;
+    if (cad1[i] == car) {
+      return i + 1;
     }
     i++;
   }
-  return pos + 1;
+  return 0;
+}
+
+int esDigito(char car) {
+  if (car >= 48 && car <= 57) {
+    return 1;
+  }else{
+    return 0;
+  }
+}
+
+int esMayuscula(char car){
+  if (car >= 65 && car <= 90) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+int esVocal(char car){
+  if (car == 65 || car == 97 || car == 85 || car == 79 || car == 73 || car == 69 || car == 101 || car == 105 || car == 111 || car == 117){
+    printf("%d\n", car);
+    return 1;
+  } else {
+    printf("%d\n", car);
+    return 0;
+  }
+}
+
+int esConsanante(char car){
+  if (((car > 65 && car <= 90) || (car > 97 && car <= 122)) && (car != 85 && car != 79 && car != 73 && car != 69 && car != 101 && car != 105 && car != 111 && car != 117)){
+    return 1;
+  } else {
+    return 0;
+  }
 }
