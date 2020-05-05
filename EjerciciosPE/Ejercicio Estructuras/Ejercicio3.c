0a6846dea2333cbe740db14f8369b348fe0631c7
HectorCosgalla <46679850+HectorCosgalla@users.noreply.github.com>
2020-05-05 10:32:06 -0500

    Update Ejercicio3.c

---
+++ EjerciciosPE/Ejercicio Estructuras/Ejercicio3.c
@@ -0,0 +1,28 @@
+#include "stdio.h"
+#define RECS 5
+
+struct recta {
+  punto p;
+  double m;
+  double b;
+};
+struct punto {
+  double x;
+  double y;
+};
+/********** PROTOTIPO DE FUNCIONES **********/
+
+int main(){
+  /********** DECLARACION DE VARIABLES **********/
+  struct recta rectas[RECS];
+
+  /********** ENTRADAS **********/
+
+  /********** PROCESOS **********/
+
+  /********** SALIDAS **********/
+
+  return 0;
+}
+
+/********** FUNCIONES **********/
